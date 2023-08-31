350c764e8d33b7ab29dfb221f54aa51e82e518bb
TheLegend-2003 <sohamdas704@gmail.com>
2023-08-31 16:12:19 +0530

    commit:6

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('This is an index view')
+    return HttpResponse('<h1 style= "color:blue">This is an index view</h1>')
+
+def details(request):
+    return HttpResponse('<h1 style= "color:beige">This is a detail view</h1>')
