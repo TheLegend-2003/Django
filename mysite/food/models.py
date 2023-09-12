7c624bb486bc73f046a55c24db45b41cbf2565b2
TheLegend-2003 <sohamdas704@gmail.com>
2023-09-12 16:43:06 +0530

    commit:26

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -6,7 +6,10 @@ class Item(models.Model):
     item_name = models.CharField(max_length=50)
     item_desc = models.CharField(max_length=300)
     item_price = models.IntegerField()
+    item_image = models.CharField(
+        max_length=500,
+        default="https://livingstonbagel.com/wp-content/uploads/2016/11/food-placeholder.jpg"
+     )
     
     def __str__(self):
         return self.item_name
-    
