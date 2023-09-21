d615ce1212e2f07eb9519991c59af47162fd8775
TheLegend-2003 <sohamdas704@gmail.com>
2023-09-21 15:52:25 +0530

    commit:42

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def register(request):
-    form = UserCreationForm()
-    
-    context = {
-        'form': form
-    }
-    
-    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('food:index')
+        
+    else:
+        form = UserCreationForm()
+        
+        context = {
+            'form': form
+        }
+      
     return render(request, 'users/register.html', context)
